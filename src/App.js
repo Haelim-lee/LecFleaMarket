dbd03ebf291902d52df62feed08b22ca10eb74c8
Junyoung Park <jaywapp16@gmail.com>
2023-01-28 23:45:48 +0900

    FAQ 기능 개발 (#12)
    * connect NavBarElements - QaBoard
    
    * create components (FAQCategories, FAQBlocks, FAQDatas)
    
    * apply radio buttons
    
    * refactor : FQA
    
    * trim about css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Screen/Home/Home';
 import MyPage from './Screen/MyPage';
+import FAQ from './Common/Components/Board/FAQ';
 import Login from './Common/Components/Login/LoginElements';
 import Navbar from './Common/Components/NavBar/NavBarElements';
 
@@ -11,7 +12,8 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={< Home />} />
+        <Route path="/FAQ" element={<FAQ />} />
         <Route path="/MyPage" element={<MyPage />} />
         <Route path="/Login" element={<Login />} />
       </Routes>
