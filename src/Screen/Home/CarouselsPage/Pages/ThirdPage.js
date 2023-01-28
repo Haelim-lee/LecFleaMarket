41a2a3dcf3a91cfde48ef62249ae03b04a4798ca
haelim_lee <86578107+Haelim-lee@users.noreply.github.com>
2023-01-28 19:10:03 +0900

    Carousels 개발 (#9)

---
+++ src/Screen/Home/CarouselsPage/Pages/ThirdPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Image, ThirdPanel } from '../../Styles/Container.style'
+
+const ThirdPage = () => {
+    return (
+        <ThirdPanel>
+            <Image src="img/book.jpg" />
+        </ThirdPanel>
+    );
+}
+
+export default ThirdPage;
