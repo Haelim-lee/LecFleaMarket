b8e574812b6bc0b81fab2a45448d80b9f79a934d
Jaywapp <jaywapp16@gmail.com>
2023-01-28 18:04:23 +0900

    connect NavBarElements - QaBoard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Screen/Home';
 import MyPage from './Screen/MyPage';
+import QaBoard from './Common/Components/Board/QaBoard';
 import Login from './Common/Components/Login/LoginElements';
 import Navbar from './Common/Components/NavBar/NavBarElements';
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={< Home />} />
+        <Route path="/FAQ" element={<QaBoard />} />
         <Route path="/MyPage" element={<MyPage />} />
         <Route path="/Login" element={<Login />} />
       </Routes>
--- src/Common/Components/Board/QaBoard/index.js
+++ src/Common/Components/Board/QaBoard/index.js
@@ -1,10 +1,14 @@
-import './style.css';
+import React from 'react';
 //import React, { createRef, useState, useEffect } from 'react';
 
-function QaBoard(props){
+function QaBoard(){
     // useEffect(() => { // 폼로드와 비슷한 개념
     //     GetPageList();
     //   }, [게시글]);
+
+    return (
+      <div>QA Board</div>
+    )
 }
 
 export default QaBoard;
